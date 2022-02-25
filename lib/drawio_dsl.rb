8dd8a93d21a579020d98f9f69aefd333257f0fcb
David Cruwys <david@ideasmen.com.au>
2022-02-25 17:02:05 +1100

    chore: opinionated gem files

--- lib/drawio_dsl.rb
+++ lib/drawio_dsl.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "drawio_dsl/version"
+require_relative 'drawio_dsl/version'
 
 module DrawioDsl
-  class Error < StandardError; end
+  # raise DrawioDsl::Error, 'Sample message'
+  Error = Class.new(StandardError)
+
   # Your code goes here...
+end
+
+if ENV['KLUE_DEBUG']&.to_s&.downcase == 'true'
+  namespace = 'DrawioDsl::Version'
+  file_path = $LOADED_FEATURES.find { |f| f.include?('drawio_dsl/version') }
+  version   = DrawioDsl::VERSION.ljust(9)
+  puts "#{namespace.ljust(35)} : #{version.ljust(9)} : #{file_path}"
 end
